38b216a3d60515973f0ca8bb47a5faf79b8c7a3a
iki1uc <entertrainer@web.de>
2026-07-30 15:09:35 +0200

    Update regel.js

--- regel.js
+++ regel.js
@@ -1,1 +1,13 @@
-regel = js meat/?.js = open 
+// regel.js
+export function MEAT_WAY(meat) {
+
+    const score =
+        (meat.start.pulse * 3) +
+        (Object.keys(meat.start.ion).length * 2) +
+        (Object.keys(meat.start.echo).length * 1);
+
+    return {
+        bestDir: score > 5 ? "SYN" : "VEC",
+        bestScore: score
+    };
+}
