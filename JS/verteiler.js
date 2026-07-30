5314625ee1b5c4842c3a118d9bcd13911a930274
iki1uc <entertrainer@web.de>
2026-07-30 14:27:30 +0200

    Create verteiler.js

---
+++ JS/verteiler.js
@@ -0,0 +1,38 @@
+W
+S
+A
+D
+Q
+E
+Y
+C
+I
+X
+O
+H
+W/meat.csv
+S/meat.csv
+A/meat.csv
+D/meat.csv
+Q/meat.csv
+E/meat.csv
+Y/meat.csv
+C/meat.csv
+I/meat.csv
+X/meat.csv
+O/meat.csv
+H/meat.csv
+const MEAT_MAP = {
+    W: "West",
+    S: "Süd",
+    A: "Alpha",
+    D: "Delta",
+    Q: "Quadrant",
+    E: "East",
+    Y: "Y-Achse",
+    C: "Curve",
+    I: "Inner",
+    X: "X-Achse",
+    O: "Outer",
+    H: "Höhe"
+};
