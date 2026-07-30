8b4f97c1846b2b485ca0268dff04c15b90257629
iki1uc <entertrainer@web.de>
2026-07-30 15:09:15 +0200

    Update MEAT.js

--- MEAT.js
+++ MEAT.js
@@ -1,10 +1,49 @@
-export function ARC_MEAT_PANEL(meat) {
+// MEAT.js
+export function MEAT(input = {}) {
+
+    const start = {
+        pulse: input.pulse ?? 0,
+        ion: input.ion ?? {},
+        mia: input.mia ?? {},
+        echo: input.echo ?? {}
+    };
+
+    const options = {
+        SYN: {
+            fusion: start.mia.fusion ?? 0,
+            resonance: start.echo.resonance ?? 0
+        },
+        VEC: {
+            pulse: start.ion.pulse ?? 0,
+            warp: start.ion.warp ?? 0,
+            charge: start.ion.charge ?? 0
+        },
+        FLX: {
+            amplitude: start.echo.amplitude ?? 0,
+            resonance: start.echo.resonance ?? 0,
+            origin: start.echo.origin ?? 0
+        },
+        ARC: {
+            lastPulse: start.pulse,
+            lastIon: start.ion,
+            lastFusion: start.mia,
+            lastEcho: start.echo
+        }
+    };
+
+    const sequence = [
+        "SYN_CORE",
+        "VEC_CORE",
+        "FLX_CORE",
+        "TEAM",
+        "ARC_CORE"
+    ];
+
     return {
-        mode: meat.mode,
-        dirs: meat.dirs,
-        map: meat.map,
-        way: MEAT_WAY(meat),
-        options: MEAT_OPTIONS(meat),
-        timestamp: meat.timestamp
+        mode: "meat",
+        start,
+        options,
+        sequence,
+        timestamp: Date.now()
     };
 }
